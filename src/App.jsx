db1f6e211110b67b86b540184700582cc87244cf
PianoBestie <dwaynedeva1996@gmail.com>
2025-04-14 23:04:54 +0530

    hjj

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
         <>
           <ScrollToTop />
           <Navbar />
-          <DashBoard2/>
+          <DashBoard/>
           <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
             <Routes location={location} key={location.pathname}>
               {/* Conditional route for admin */}
