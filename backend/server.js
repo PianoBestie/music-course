22549649c77ac2fb8f6b4f7de6777848b17b3e8d
PianoBestie <dwaynedeva1996@gmail.com>
2025-04-15 11:43:41 +0530

    asdF

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: ['https://pianobestie.github.io', 'https://pianobestie.github.io/music-course'],
   credentials: true
 }));
 
